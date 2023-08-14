90c073e13e66b1049a53277d152d841f7d5a8121
Adolfo J. Temoche Guevara <ajt21397@hotmail.com>
2023-08-13 23:14:08 -0700

    added the api endpoints for all

--- index.js
+++ index.js
@@ -7,49 +7,57 @@ const app = express();
 
 const PORT = 8080;
 
+app.use(express.static('public'));
+
 // GET - / - returns homepage
-app.get('/', (req, res) => {
+app.get('/', (req, res,) => {
     // serve up the public folder as static index.html file
+    res.sendFile(__dirname + '/index.html')
 
 });
 
 // hello world route
-app.get('/api', (req, res) => {
+app.get('/api', (req, res, _next) => {
     res.send('Hello World!');
 });
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    res.send(pets);
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
+    const {owner} = req.query;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.send(pet);
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
+    const {name} = req.query;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
+    res.send(pet);
 
 });
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+    console.log(`Server is listening on port https://localhost:${PORT}`);
 });
 
 module.exports = app;
