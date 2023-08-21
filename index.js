8bc3ce183cd01fea3c41b7d6b7fac02c3e2a67eb
Raymart Domagas <raymartdomagas@Raymarts-MacBook-Air.local>
2023-08-21 18:30:22 -0400

    added code

--- index.js
+++ index.js
@@ -7,45 +7,48 @@ const app = express();
 
 const PORT = 8080;
 
+// Server our index.html
+app.use(express.static("public"));
+
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + "index.html");
 });
 
 // hello world route
 app.get('/api', (req, res) => {
-    res.send('Hello World!');
+    res.send("<h1>Hello, World</h1>");
 });
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.json(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner= req.params.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name= req.params.name;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet);
 });
 
 app.listen(PORT, () => {
